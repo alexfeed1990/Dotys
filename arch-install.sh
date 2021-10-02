c4030961ad902572e61e277e5c08ebd2d4063d5f
Andrew <76942911+alexfeed1990@users.noreply.github.com>
2021-10-02 20:39:03 +0300

    asdasd

--- arch-install.sh
+++ arch-install.sh
@@ -8,7 +8,7 @@ sudo git clone https://aur.archlinux.org/yay-git.git
 sudo chown -R $USER:$USER ./yay-git
 cd yay-git
 makepkg -si
-yay -S betterlockscreen deadbeef-git trayer-srg ttf-font-awesome-4
+yay -S betterlockscreen trayer-srg ttf-font-awesome-4
 betterlockscreen -u ~/Wallpaper.jpg
 echo "Use nitrogen after getting into xmonad to set the wallpaper, as stated in the readme."
 sudo systemctl enable sddm
