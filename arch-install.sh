825433de4e50f62fffb48238e7f7bc9c742a2b5f
Andrew <76942911+alexfeed1990@users.noreply.github.com>
2021-10-02 16:48:07 +0300

    asdasd

--- arch-install.sh
+++ arch-install.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/alexfeed1990/dotys
 cd dotys
-sudo pacman -Syu noto-fonts-emoji xmonad xmonad-contrib xmobar alsa-utils pulseaudio pulsemixer picom flameshot dunst rofi spotify alacritty firefox arandr feh nitrogen sddm xorg-xrandr xorg-xsetroot xorg-server xorg git
+sudo pacman -Syu ttf-font-awesome noto-fonts-emoji xmonad xmonad-contrib xmobar alsa-utils pulseaudio pulsemixer picom flameshot dunst rofi spotify alacritty firefox arandr feh nitrogen sddm xorg-xrandr xorg-xsetroot xorg-server xorg git
 cp -a * . ~/
 pacman -S --needed git base-devel
 cd /opt
@@ -9,6 +9,7 @@ sudo chown -R $USER:$USER ./yay-git
 cd yay-git
 makepkg -si
 yay -S betterlockscreen deadbeef-git trayer-srg
+betterlockscreen -u ~/Wallpaper.jpg
 sudo systemctl enable sddm
 sudo systemctl enable dunst
 echo "Everything should be setup as intended. Enjoy your setup!"
