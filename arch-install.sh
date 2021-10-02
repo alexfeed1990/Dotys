bd7dab282445c21411408539cdb19f84bcd16fbe
Andrew <76942911+alexfeed1990@users.noreply.github.com>
2021-10-02 16:46:25 +0300

    arch instoll

---
+++ arch-install.sh
@@ -0,0 +1,15 @@
+git clone https://github.com/alexfeed1990/dotys
+cd dotys
+sudo pacman -Syu noto-fonts-emoji xmonad xmonad-contrib xmobar alsa-utils pulseaudio pulsemixer picom flameshot dunst rofi spotify alacritty firefox arandr feh nitrogen sddm xorg-xrandr xorg-xsetroot xorg-server xorg git
+cp -a * . ~/
+pacman -S --needed git base-devel
+cd /opt
+sudo git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R $USER:$USER ./yay-git
+cd yay-git
+makepkg -si
+yay -S betterlockscreen deadbeef-git trayer-srg
+sudo systemctl enable sddm
+sudo systemctl enable dunst
+echo "Everything should be setup as intended. Enjoy your setup!"
+exit
